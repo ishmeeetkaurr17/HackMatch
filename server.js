e5ed5531b7c0a2f627f7a7979859c129f0e12bdd
Ishmeet Kaur <ishmeeetkaurr@gmail.com>
2026-06-28 15:12:54 +0530

    Use Render port

--- server.js
+++ server.js
@@ -52,6 +52,8 @@ teams.push(newTeam);
 
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
